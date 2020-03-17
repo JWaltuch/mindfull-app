8bef4f0d8bdf600d0090f52bdd12d02bdc194cb4
JWaltuch <jenna.waltuch@gmail.com>
2020-03-17 14:08:54 -0400

    feat: added journal pages. mvp status

--- script.js
+++ script.js
@@ -2,15 +2,10 @@ if (document.getElementById('index')) {
   window.onload = function() {
     setTimeout(() => {
       window.location = '/forms/bored.html';
-    }, 5000);
+    }, 2000);
   };
 }
 
-function redirect(event, nextPage) {
-  event.preventDefault();
-  if (nextPage === 'index') {
-    window.location = `/${nextPage}.html`;
-  } else {
-    window.location = `/forms/${nextPage}.html`;
-  }
+if (document.getElementById('journal')) {
+  window.onload = function() {};
 }
