3ff24d80a65b90e52947809337c687fc57bbca63
JWaltuch <jenna.waltuch@gmail.com>
2020-03-17 15:06:53 -0400

    fix: added app name to links for github deployment

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 if (document.getElementById('index')) {
   window.onload = function() {
     setTimeout(() => {
-      window.location = '/forms/bored.html';
+      window.location = '/mindfull-app/forms/bored.html';
     }, 2000);
   };
 }
